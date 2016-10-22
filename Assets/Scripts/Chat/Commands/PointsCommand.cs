edb392c57872300cd7e2639ce185860fa72b1ee3
XLIII <xliii@mail.com>
2016-10-22 22:48:30 +0300

    !points command

---
+++ Assets/Scripts/Chat/Commands/PointsCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class PointsCommand : ChatCommand {
+	public override void process(User user, string[] args, Action<string> callback)
+	{
+		if (args.Length == 0)
+		{
+			callback("You have " + user.points + " points");
+			return;
+		}
+
+		var target = UserManager.instance.GetUser(args[0]);
+
+		if (target == null)
+		{
+			callback("User \"" + args[0] + "\" does not exist");
+			return;
+		}
+
+		callback(target.username + " has " + target.points + " points");
+	}
+
+	public override int cooldown()
+	{
+		return 0;
+	}
+
+	public override string command()
+	{
+		return "!points";
+	}
+}
